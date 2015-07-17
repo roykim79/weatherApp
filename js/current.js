fda3675139bb9f90a0a6a0cd653a4e2442791833
Roy Kim <rykim79@gmail.com>
2015-07-16 18:54:53 -0700

    commit

---
+++ js/current.js
@@ -0,0 +1,39 @@
+{
+    "coord": {
+        "lon": -71.06,
+        "lat": 42.36
+    },
+    "weather": [{
+        "id": 803,
+        "main": "Clouds",
+        "description": "broken clouds",
+        "icon": "04n"
+    }],
+    "base": "cmc stations",
+    "main": {
+        "temp": 66.51,
+        "pressure": 1011,
+        "humidity": 94,
+        "temp_min": 63,
+        "temp_max": 75.2
+    },
+    "wind": {
+        "speed": 3.24,
+        "deg": 23.0037
+    },
+    "clouds": {
+        "all": 75
+    },
+    "dt": 1437019973,
+    "sys": {
+        "type": 1,
+        "id": 1801,
+        "message": 0.0116,
+        "country": "US",
+        "sunrise": 1437038493,
+        "sunset": 1437092317
+    },
+    "id": 4930956,
+    "name": "Boston",
+    "cod": 200
+}
--- js/timeStamp.js
+++ js/timeStamp.js
@@ -1,12 +1,24 @@
-function  FormatTime(timeStamp){
+function FormatTime(timeStamp){
 	this.time = new Date(timeStamp);
 }
 
 FormatTime.prototype.getDayName = function(timeStamp){
-	var time = new Date(timeStamp);
-
-	var days = ['Sun', 'Mon', 'Tues', 'Weds', 'Thur', 'Fri', 'Sat'];
+	var time = new Date(timeStamp),
+		days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
 	return days[ time.getDay() ];
 }
 
+FormatTime.prototype.getMonthAndDay = function(timeStamp){
+	var time = new Date(timeStamp),
+		months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+	return (months[ time.getMonth() ] + " " + time.getDate());
+}
+
+FormatTime.prototype.getTimeOfDay = function(timeStamp){
+	var arr = timeStamp.split(" "),
+		arr2 = arr[0].split(":").splice(0, 2).join(":");
+
+	return ( arr2 + " " + arr[1] );
+}
